ae7a100081ed33c9a06ea724d07ec2163428ae24
chughes <christopher.hughes@inria.fr>
2022-04-01 16:03:46 +0200

    Notes on polynomial associativity

--- lean/poly_assoc.lean
+++ lean/poly_assoc.lean
@@ -1,6 +1,6 @@
 import linear_algebra.finsupp
 
-variables {R α M N P : Type} [comm_ring R]
+variables {R α β γ M N P : Type} [comm_ring R]
 variables [add_comm_group M] [module R M]
 variables [add_comm_group N] [module R N]
 variables [add_comm_group P] [module R P]
@@ -16,24 +16,40 @@ def free_module : Type := finsupp α R
 
 variables {R α}
 
-def lift (f : α → M) : free_module R α →ₗ[R] M :=
+def extend (f : α → M) : free_module R α →ₗ[R] M :=
 finsupp.total _ _ _ f
 
 def X (a : α) : free_module R α := finsupp.single a 1
 
-@[simp] lemma lift_X (f : α → M) (a : α) : lift f (X a : free_module R α) = f a :=
-by simp [lift, X]
+@[simp] lemma extend_X (f : α → M) (a : α) : extend f (X a : free_module R α) = f a :=
+by simp [extend, X]
 
 @[ext] lemma hom_ext {f g : free_module R α →ₗ[R] M} (h : ∀ a, f (X a) = g (X a)) : f = g :=
 by ext; simp [*, X] at *
 
-@[simp] lemma lift_comp (f : M →ₗ[R] N) (g : α → M) :
-  f.comp (lift g) = lift (λ x, f (g x)) :=
-by ext; simp
+variable (R)
+def map (f : α → β) : free_module R α →ₗ[R] free_module R β :=
+extend (X ∘ f)
 
-@[simp] lemma lift_comp_apply (f : M →ₗ[R] N) (g : α → M) (x : free_module R α) :
-  f (lift g x) = lift (λ x, f (g x)) x :=
-by rw [← lift_comp]; refl
+variable {R}
+lemma map_id  : map R (id : α → α) = linear_map.id :=
+hom_ext (λ a, by rw [map, extend_X]; refl)
+
+lemma map_comp (f : α → β) (g : β → γ) : map R (g ∘ f) = (map R g).comp (map R f) :=
+hom_ext begin
+  rw [map, map, map],
+  intro i,
+  rw [extend_X, linear_map.comp_apply, extend_X, extend_X],
+
+end
+
+@[simp] lemma extend_comp (f : M →ₗ[R] N) (g : α → M) :
+  f.comp (extend g) = extend (λ x, f (g x)) :=
+by ext; simp only [linear_map.coe_comp, function.comp_app, extend_X]
+
+@[simp] lemma extend_comp_apply (f : M →ₗ[R] N) (g : α → M) (x : free_module R α) :
+  f (extend g x) = extend (λ x, f (g x)) x :=
+by rw [← extend_comp]; refl
 
 def lcomp : (M →ₗ[R] N) →ₗ[R] (P →ₗ[R] M) →ₗ[R] (P →ₗ[R] N) :=
 { to_fun := λ f,
@@ -62,9 +78,11 @@ def swap : (M →ₗ[R] N →ₗ[R] P) →ₗ[R] (N →ₗ[R] M →ₗ[R] P) :=
   map_add' := λ _ _, rfl,
   map_smul' := λ _ _, rfl }
 
-@[simp] lemma lift_apply (f : α → M →ₗ[R] N) (x : free_module R α) (m : M) :
-  lift f x m = lift (λ a, f a m) x :=
-show swap (lift f) m x = lift (λ a, f a m) x,
+
+
+@[simp] lemma extend_apply (f : α → M →ₗ[R] N) (x : free_module R α) (m : M) :
+  extend f x m = extend (λ a, f a m) x :=
+show swap (extend f) m x = extend (λ a, f a m) x,
 begin
   refine congr_fun _ _,
   refine congr_arg _ _,
@@ -74,15 +92,15 @@ end
 
 variable {M}
 
-attribute [irreducible] free_module lift X
+attribute [irreducible] free_module extend X
 
 def mul : free_module R ℕ →ₗ[R] free_module R ℕ →ₗ[R] free_module R ℕ :=
-lift (λ n, lift (λ m, X (n + m)))
+extend (λ n, extend (λ m, X (n + m)))
 
 -- free_module R ℕ →ₗ[R] free_module R ℕ →ₗ[R] free_module R ℕ →ₗ[R] free_module R ℕ
 
 lemma mul_assoc (p q r : free_module R ℕ) : mul (mul p q) r = mul p (mul q r) :=
--- by simp [mul, add_assoc]
+--by simp only [mul, add_assoc, extend_apply, extend_comp_apply, extend_X]
 
 -- #print mul_assoc
 
@@ -96,8 +114,8 @@ begin
   intro,
   apply hom_ext,
   intro,
-  dsimp [lcomp, lcompop, mul],
-  rw [lift_X, lift_X, lift_X, lift_X, lift_X, lift_X, lift_X, add_assoc],
+  dsimp [mul],
+  rw [extend_X, extend_X, extend_X, extend_X, extend_X, extend_X, extend_X, add_assoc],
 end
 
 lemma mul_comm (p q : free_module R ℕ) : mul p q = mul q p :=
