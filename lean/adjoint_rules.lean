7da672891e29c0434909abad315ca6efefcbb989
chughes <christopher.hughes@inria.fr>
2023-02-21 16:38:18 +0000

    update adjoint_rules

--- lean/adjoint_rules.lean
+++ lean/adjoint_rules.lean
@@ -1,123 +1,99 @@
 import category_theory.adjunction
 
-open category_theory
+namespace category_theory
 
-variables {C D E : Type} [category.{0} C] [category.{0} D] [category.{0} E]
+variables {C D : Type*} [category C] [category D]
 
-section unit_extend
-
-structure unit_extend (F : C ⥤ D) :=
+structure unit_desc (F : C ⥤ D) :=
 ( obj : D → C )
 ( unit : Π (X : D), X ⟶ F.obj (obj X) )
-( extend : Π {X Y}, (X ⟶ F.obj Y) → (obj X ⟶ Y))
-( unit_comp_extend : Π {X Y} (f : X ⟶ F.obj Y), unit X ≫ F.map (extend f) = f )
-( extend_unit : Π {X Y} (f : obj X ⟶ Y), extend (unit X ≫ F.map f) = f )
+( desc : Π {X Y}, (X ⟶ F.obj Y) → (obj X ⟶ Y))
+( unit_comp_desc : Π {X Y} (f : X ⟶ F.obj Y), unit X ≫ F.map (desc f) = f )
+( desc_unit : Π {X Y} (f : obj X ⟶ Y), desc (unit X ≫ F.map f) = f )
 
-structure counit_restrict (F : C ⥤ D) :=
+variables {F : C ⥤ D} (ud : unit_desc F)
+
+private lemma unit_desc_ext
+  {X : D} {Y : C} {f g : ud.obj X ⟶ Y}
+  (H : ud.unit X ≫ F.map f = ud.unit X ≫ F.map g) : f = g :=
+by rw [← ud.desc_unit f, H, ud.desc_unit]
+
+def unit_desc.to_functor : D ⥤ C :=
+{ obj := ud.obj,
+  map := λ X Y f, ud.desc (f ≫ ud.unit Y),
+  map_id' := λ X, by rw [category.id_comp, ← category.comp_id (ud.unit X), ← F.map_id,
+      ud.desc_unit],
+  map_comp' := λ X Y Z f g, unit_desc_ext ud $
+    by rw [F.map_comp, ud.unit_comp_desc, ← category.assoc, ud.unit_comp_desc,
+      category.assoc, category.assoc, ud.unit_comp_desc] }
+
+def unit_desc.adjunction :
+  ud.to_functor ⊣ F :=
+adjunction.mk_of_hom_equiv
+{ hom_equiv := λ X Y,
+  { to_fun := λ f, ud.unit X ≫ F.map f,
+    inv_fun := λ f, ud.desc f,
+    left_inv := λ f, ud.desc_unit f,
+    right_inv := λ f, ud.unit_comp_desc f },
+  hom_equiv_naturality_left_symm' :=
+    begin
+      intros X' X Y f g,
+      dsimp [unit_desc.to_functor],
+      apply unit_desc_ext ud,
+      rw [ud.unit_comp_desc, F.map_comp, ← category.assoc,
+        ud.unit_comp_desc, category.assoc, ud.unit_comp_desc],
+    end,
+  hom_equiv_naturality_right' :=
+    begin
+      intros X Y Y' f g,
+      dsimp [unit_desc.to_functor],
+      rw [F.map_comp, category.assoc]
+    end }
+
+structure counit_lift (F : C ⥤ D) :=
 ( obj : D → C )
 ( counit : Π (X : D), F.obj (obj X) ⟶ X )
-( restrict : Π {X Y}, (F.obj X ⟶ Y) → (X ⟶ obj Y))
-( restrict_comp_counit : Π {X Y} (f : F.obj X ⟶ Y), F.map (restrict f) ≫ counit Y = f )
-( restrict_counit : Π {X Y} (f : X ⟶ obj Y), restrict (F.map f ≫ counit Y) = f )
+( lift : Π {X Y}, (F.obj X ⟶ Y) → (X ⟶ obj Y))
+( lift_comp_counit : Π {X Y} (f : F.obj X ⟶ Y), F.map (lift f) ≫ counit Y = f )
+( lift_counit : Π {X Y} (f : X ⟶ obj Y), lift (F.map f ≫ counit Y) = f )
 
-variables {F : C ⥤ D} (ue : unit_extend F) (cr : counit_restrict F)
+variable (cl : counit_lift F)
 
-private lemma unit_extend_ext
-  {X : D} {Y : C} {f g : ue.obj X ⟶ Y}
-  (H : ue.unit X ≫ F.map f = ue.unit X ≫ F.map g) : f = g :=
-by rw [← ue.extend_unit f, H, ue.extend_unit]
+private lemma counit_lift_ext
+  {X : C} {Y : D} {f g : X ⟶ cl.obj Y}
+  (H : F.map f ≫ cl.counit Y = F.map g ≫ cl.counit Y) : f = g :=
+by rw [← cl.lift_counit f, H, cl.lift_counit]
 
-private lemma counit_restrict_ext
-  {X : C} {Y : D} {f g : X ⟶ cr.obj Y}
-  (H : F.map f ≫ cr.counit Y = F.map g ≫ cr.counit Y) : f = g :=
-by rw [← cr.restrict_counit f, H, cr.restrict_counit]
+def counit_lift.to_functor : D ⥤ C :=
+{ obj := cl.obj,
+  map := λ X Y f, cl.lift (cl.counit X ≫ f),
+  map_id' := λ X, by rw [category.comp_id, ← category.id_comp (cl.counit X), ← F.map_id,
+      cl.lift_counit],
+  map_comp' := λ X Y Z f g, counit_lift_ext cl $
+    by rw [F.map_comp, cl.lift_comp_counit, category.assoc, cl.lift_comp_counit,
+      ← category.assoc, ← category.assoc, cl.lift_comp_counit] }
 
-def left_adjoint_of_unit_extend : D ⥤ C :=
-{ obj := ue.obj,
-  map := λ X Y f, ue.extend (f ≫ ue.unit Y),
-  map_id' := λ X, by rw [category.id_comp, ← category.comp_id (ue.unit X), ← F.map_id,
-      ue.extend_unit],
-  map_comp' := λ X Y Z f g, unit_extend_ext ue $
-    by rw [F.map_comp, ue.unit_comp_extend, ← category.assoc, ue.unit_comp_extend,
-      category.assoc, category.assoc, ue.unit_comp_extend] }
-
-def right_adjoint_of_counit_restrict : D ⥤ C :=
-{ obj := cr.obj,
-  map := λ X Y f, cr.restrict (cr.counit X ≫ f),
-  map_id' := λ X, by rw [category.comp_id, ← category.id_comp (cr.counit X), ← F.map_id,
-      cr.restrict_counit],
-  map_comp' := λ X Y Z f g, counit_restrict_ext cr $
-    by rw [F.map_comp, cr.restrict_comp_counit, category.assoc, cr.restrict_comp_counit,
-      ← category.assoc, ← category.assoc, cr.restrict_comp_counit] }
-
-def adjunction_of_unit_extend :
-  left_adjoint_of_unit_extend ue ⊣ F :=
+def counit_lift.adjunction :
+  F ⊣ cl.to_functor :=
 adjunction.mk_of_hom_equiv
 { hom_equiv := λ X Y,
-  { to_fun := λ f, ue.unit X ≫ F.map f,
-    inv_fun := λ f, ue.extend f,
-    left_inv := λ f, ue.extend_unit f,
-    right_inv := λ f, ue.unit_comp_extend f },
-  hom_equiv_naturality_left_symm' :=
-    begin
-      intros X' X Y f g,
-      dsimp [left_adjoint_of_unit_extend],
-      apply unit_extend_ext ue,
-      rw [ue.unit_comp_extend, F.map_comp, ← category.assoc,
-        ue.unit_comp_extend, category.assoc, ue.unit_comp_extend],
-    end,
-  hom_equiv_naturality_right' :=
-    begin
-      intros X Y Y' f g,
-      dsimp [left_adjoint_of_unit_extend],
-      rw [F.map_comp, category.assoc]
-    end }
-
-def adjunction_of_counit_restrict :
-  F ⊣ right_adjoint_of_counit_restrict cr :=
-adjunction.mk_of_hom_equiv
-{ hom_equiv := λ X Y,
-  { to_fun := λ f, cr.restrict f,
-    inv_fun := λ f, F.map f ≫ cr.counit Y,
-    left_inv := λ f, cr.restrict_comp_counit f,
-    right_inv := λ f, cr.restrict_counit f },
+  { to_fun := λ f, cl.lift f,
+    inv_fun := λ f, F.map f ≫ cl.counit Y,
+    left_inv := λ f, cl.lift_comp_counit f,
+    right_inv := λ f, cl.lift_counit f },
   hom_equiv_naturality_left_symm' :=
     begin
       intros X Y Y' f g,
-      dsimp [right_adjoint_of_counit_restrict],
+      dsimp [counit_lift.to_functor],
       rw [F.map_comp, category.assoc]
     end,
   hom_equiv_naturality_right' :=
     begin
       intros X' X Y f g,
-      dsimp [right_adjoint_of_counit_restrict],
-      apply counit_restrict_ext cr,
-      rw [cr.restrict_comp_counit, F.map_comp, category.assoc,
-        cr.restrict_comp_counit, ← category.assoc, cr.restrict_comp_counit],
+      dsimp [counit_lift.to_functor],
+      apply counit_lift_ext cl,
+      rw [cl.lift_comp_counit, F.map_comp, category.assoc,
+        cl.lift_comp_counit, ← category.assoc, cl.lift_comp_counit],
     end }
 
-end unit_extend
-
-section rewrite_rules
-
-variables (F : C ⥤ D) (RF : counit_restrict F) (G : D ⥤ E) (RG : counit_restrict G)
-
--- restrict_counit
-
-lemma restrict_counit' {X : D} :
-  RF.restrict (RF.counit X) = 𝟙 (RF.obj X) :=
-by rw [← category.id_comp (RF.counit X), ← F.map_id, RF.restrict_counit]
-
-lemma comp_restrict {X Y : C} {Z : D} (f : X ⟶ Y) (g : F.obj Y ⟶ Z) :
-  f ≫ RF.restrict g = RF.restrict (F.map f ≫ g) :=
-((adjunction_of_counit_restrict RF).hom_equiv_naturality_left f g).symm
-
--- lemma restrict_counit' {X : E} :
---   RG.restrict (G.map (RF.counit (RG.obj X)) ≫ RG.counit X) = RF.counit (RG.obj X) :=
--- begin
---   rw [RG.restrict_counit]
-
--- end
-
-
-end rewrite_rules
+end category_theory
